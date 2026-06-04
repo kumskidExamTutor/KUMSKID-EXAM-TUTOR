7005cab399ea889e459967db6f46a67e277b40ea
kumskidExamTutor <kumskidfootwear@gmail.com>
2026-06-04 04:37:34 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://wjrjoinhxybzkebyahgx.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_eECjsU4kBBc7r7Y0nKiNnA_G0wJwkNn";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
